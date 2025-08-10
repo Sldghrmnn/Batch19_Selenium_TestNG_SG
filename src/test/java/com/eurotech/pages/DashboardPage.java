8aa4132e813ebc8bdadef47474e74d2372d0499d
SeldaGehrmann <seldagehrmann75@gmail.com>
2025-08-10 15:55:54 +0200

    Initial commit

---
+++ src/test/java/com/eurotech/pages/DashboardPage.java
@@ -0,0 +1,24 @@
+package com.eurotech.pages;
+
+import com.eurotech.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.awt.*;
+
+public class DashboardPage extends BasePage {
+
+
+   // public DashboardPage(){PageFactory.initElements(Driver.get(),this);}
+
+
+    @FindBy(xpath = "//h2")
+    public WebElement userName;
+
+    @FindBy(css=".breadcrumb>li:nth-of-type(2)")
+    public WebElement pageTitleLeftTop;
+
+
+
+}
